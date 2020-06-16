f4f04b8eff36489bd752f98c9069f0e06de97213
Jared <jaredjgebel@gmail.com>
2020-06-15 23:01:50 -0400

    Add mongoose connection, delete erroneous code

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
-require("reflect-metadata");
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-// const createConnection = require('typeorm');
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+const port = process.env.PORT || 3000;
+const host = "localhost";
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -18,5 +20,32 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+const db = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Database has been successfully connected.");
+});
+
+// mongoose.connect(process.env.MONGODB_URI, function () {
+//   console.log("Connected to database");
+// });
+
+let args;
+process.env.NODE_ENV === "production" ? (args = [port]) : (args = [port, host]);
+
+args.push(() => {
+  console.log(`Listening: http://${host}:${port}\n`);
+});
+
+// If file is being run directly, start the server
+if (require.main === module) {
+  app.listen.apply(app, args);
+}
 
 module.exports = app;
