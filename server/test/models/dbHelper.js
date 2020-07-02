06a99a8eb20f68001b9c699bcffe560a83b2a8da
Jared <jaredjgebel@gmail.com>
2020-07-02 01:11:40 -0400

    Create user model tests

---
+++ server/test/models/dbHelper.js
@@ -0,0 +1,28 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname + "../../../.env") });
+const mongoose = require("mongoose");
+
+before((done) => {
+  mongoose.connect(process.env.MONGODB_URI_TEST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connection
+    .once("open", () => {
+      console.log("Test connection opened");
+      done();
+    })
+    .on("error", () => {
+      console.warn("Database connection error: ", error);
+      done();
+    });
+});
+
+after((done) => {
+  mongoose.connection.close((err) => {
+    if (err) console.error(err);
+    console.log("Database connection closed.");
+    done();
+  });
+});
---
+++ server/test/models/user.test.js
@@ -0,0 +1,52 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname + "../../../.env") });
+// require("./dbHelper");
+
+const expect = require("chai").expect;
+const faker = require("faker");
+const User = require("../../models/user");
+
+describe("User model", () => {
+  it("should require first name on creation", async () => {
+    const noFirstName = new User({
+      lastName: faker.name.lastName(),
+    });
+
+    let userDoc;
+
+    try {
+      userDoc = await noFirstName.save();
+    } catch (err) {
+      expect(err._message).to.equal("User validation failed");
+    }
+
+    expect(userDoc).to.be.undefined;
+  });
+
+  it("should require last name on creation", async () => {
+    const noLastName = new User({
+      firstName: faker.name.firstName(),
+    });
+
+    let userDoc;
+
+    try {
+      userDoc = await noLastName.save();
+    } catch (err) {
+      expect(err._message).to.equal("User validation failed");
+    }
+
+    expect(userDoc).to.be.undefined;
+  });
+
+  it("should have one empty book upon creation", () => {
+    const user = new User({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+    });
+
+    expect(user.books.length).to.equal(1);
+    expect(user.books[0].title).to.equal("");
+  });
+});
