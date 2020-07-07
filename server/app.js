6ad8744cd22c50a5230d9c06f69a2c742053af9c
Jared <jaredjgebel@gmail.com>
2020-07-07 17:43:28 -0400

    Remove view layer and public folder

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -16,7 +15,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
