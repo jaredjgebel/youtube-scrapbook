6f0c6279b8ac51f52eec57ed3b47971bd452517d
Jared <jaredjgebel@gmail.com>
2020-09-06 21:24:57 -0400

    Create private routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Flex } from "@chakra-ui/core";
-
 import { useAuth0 } from "@auth0/auth0-react";
+
+import PrivateRoute from "./auth/PrivateRoute";
 import LoginButton from "./components/buttons/LoginButton";
 import LogoutButton from "./components/buttons/LogoutButton";
 import FrontPage from "./components/views/FrontPage";
 import Home from "./components/views/Home";
+import Profile from "./components/views/Profile";
 
 const App = () => {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -16,12 +18,13 @@ const App = () => {
       {isLoading ? (
         "Loading..."
       ) : (
-        <Flex>
+        <Flex direction="column">
           {isAuthenticated ? <LogoutButton /> : <LoginButton />}
 
           <Switch>
             <Route path="/" exact component={FrontPage} />
-            <Route path="/books" component={Home} />
+            <PrivateRoute path="/books" component={Home} />
+            <PrivateRoute path="/profile" component={Profile} />
           </Switch>
         </Flex>
       )}
