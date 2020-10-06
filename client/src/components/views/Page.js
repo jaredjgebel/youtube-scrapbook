eeaa6c36f21dbf03d3f67bafc3fccce33e96b229
Jared <jaredjgebel@gmail.com>
2020-10-06 10:33:53 -0400

    Page button styling

--- client/src/components/views/Page.js
+++ client/src/components/views/Page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import { Flex, Grid, IconButton, Text } from "@chakra-ui/core";
+import ButtonWithStyle from "../buttons/ButtonWithStyle";
 
 const Page = ({ number, video, pageCount, setVisible }) => (
   <Grid
@@ -8,7 +9,8 @@ const Page = ({ number, video, pageCount, setVisible }) => (
     height="95%"
     width="95%"
     gridTemplateRows="1.2fr 1fr"
-    padding={2}
+    paddingX={[2, 4, 6, 8]}
+    paddingY={2}
     boxShadow="0 0 10px gray"
     overflowY="auto"
   >
@@ -16,7 +18,23 @@ const Page = ({ number, video, pageCount, setVisible }) => (
       <ReactPlayer url={video.link} />
     </Flex>
     <Flex flexDirection="column" justifyContent="space-between">
-      <Text padding={4}>{video.notes}</Text>
+      <Flex flexDirection="column">
+        <Text padding={4}>{video.notes}</Text>
+        <Flex justifyContent="space-around">
+          <ButtonWithStyle
+            margin={2}
+            width={["100px", "100px", "100px", "125px"]}
+          >
+            Edit
+          </ButtonWithStyle>
+          <ButtonWithStyle
+            margin={2}
+            width={["100px", "100px", "100px", "125px"]}
+          >
+            Delete
+          </ButtonWithStyle>
+        </Flex>
+      </Flex>
       <Flex justifyContent="space-between" width="100%">
         <IconButton
           aria-label="Previous page"
